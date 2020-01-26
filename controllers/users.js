1971aa659aabe0fd8b6d3ccd4015c90bc15c2fcf
NIkhil Gautam <nikhilgautam992@gmail.com>
2020-01-27 00:22:09 +0530

    added registration...

--- controllers/users.js
+++ controllers/users.js
@@ -5,43 +5,60 @@ const User = require('../models/user');
 exports.addUsers = (req, res, next) => {
     let user = req.query;
 
-    User.findOne({
-        name: user.name
-    }, (result) => {
-        if (!result) {
-            const newUser = new User({
-                name: user.name,
-                email: user.email,
-                phone: parseInt(user.phone, 10),
-                address: user.address,
-                sos: {
-                    isTrue: false,
-                    previous_sos: []
-                },
-                password: user.password,
-                emergency_contact: []
-            });
+    console.log(user.email);
 
-            newUser.save().then((result) => {
-                res.json({
-                    success: true,
-                    uid: newUser._id,
-                    msg: "User added Sucessfully"
+    User.findOne({
+        email: user.email
+    }, (err, result) => {
+        if (!err) {
+
+            if (!result) {
+                const newUser = new User({
+                    name: user.name,
+                    email: user.email,
+                    phone: parseInt(user.phone, 10),
+                    address: user.address,
+                    sos: {
+                        isTrue: false,
+                        previous_sos: []
+                    },
+                    password: user.password,
+                    emergency_contact: []
                 });
-            }).catch(err => {
+
+                newUser.save().then((result) => {
+                    res.json({
+                        success: true,
+                        uid: newUser._id,
+                        msg: "User added Sucessfully"
+                    });
+                }).catch(err => {
+                    res.json({
+                        success: false,
+                        msg: "Failed to add user",
+                        err
+                    });
+                });
+
+            } else {
                 res.json({
                     success: false,
-                    msg: "Failed to add user",
-                    err
+                    uid: result._id,
+                    msg: "User already exits",
                 });
-            });
+            }
+
+            // res.json(result);
 
         } else {
             res.json({
                 success: false,
-                uid: result._id,
                 msg: "User already exits",
+                err
             });
         }
+
+
+
     })
 }
